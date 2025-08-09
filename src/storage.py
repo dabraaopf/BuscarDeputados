0cffc506a3b4643754fb3361040d53e2060802ae
daniel <daniel@seneca>
2025-08-09 15:07:54 -0300

    Start the storage process

--- src/storage.py
+++ src/storage.py
@@ -1,10 +1,16 @@
 from os import path, getcwd
 import logging 
 import sqlite3
+from enums import Enum, auto
+
+class PolicitianStatus(Enum):
+    ACTIVE = auto()
+    INACTIVE = auto()
+    NO_MANDATE = auto()
 
 class Storage():
-    def __init__(self, stora_path="", log=None):
-        self._storage_path = stora_path 
+    def __init__(self, storage_path="", log=None):
+        self._storage_path = storage_path 
         self._connection = None
         self._cursor = None
         self._log = log
@@ -12,8 +18,41 @@ class Storage():
         self._log_name = ""
 
     def _get_connection(self):
+        if not path.exists(self._storage_path):
+            self._create_tables()
         if isinstance(self._connection, sqlite.Connection):
             return self._connection
+        self._connection = sqlite3.connect(self._storage_path)
+        return self._connection
+
+    def _create_tables():
+        log = self._get_logger()
+        self._connection = sqlite3.connect(self._storage_path)
+        sql = """CREATE TABLE IF NOT EXISTS Politicians(
+            id INT PRIMARY KEY,
+            name varchar(150) NOT NULL,
+            email varchar(250) UNIQUE,
+            state varchar(2) NOT NULL,
+            start_mandate INT,
+            end_mandate INT,
+            status INT 
+        )
+        """
+        cursor = self._connection.cursor()
+        try:
+            cursor.execute(sql)
+        except Exception as e:
+            log.error(f'_create_tables: failed to create the table {e} {type(e)}') 
+            raise e
+        else:
+            sql = """CREATE TABLE IF NOT EXISTS Searchs(
+                lastUpdate INT, 
+                status varchar(1)
+            )
+            """
+            cursor.execute(sql)
+            return True
+        return False
         
     def _get_logger(self):
         if isinstance(self._log, logging.Logger):
@@ -25,4 +64,12 @@ class Storage():
         self._internal_log = logging.getLogger("STORAGE")
         loggin.basicConfig(filename=self._log_name, level=logging.ERROR)
         return self._internal_log
+
+    def add_politician(self, politician):
+        
+        pass
+
+    def add_search_status(self, status):
+        pass
+
 
